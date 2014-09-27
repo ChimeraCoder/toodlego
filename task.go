3d930cc65fecbbf3db25686d81a6860cf1981f8f
aditya <dev@chimeracoder.net>
2014-09-27 15:55:13 -0400

    Add task.go

---
+++ task.go
@@ -0,0 +1,21 @@
+package toodledo
+
+type TaskResponse struct {
+	Meta  TaskResponseMeta
+	Tasks []Task
+}
+
+type TaskResponseMeta struct {
+	Num   int64 `json:"num"`
+	Total int64 `json:"total"`
+}
+
+type Task struct {
+	Completed int64  `json:"completed"`
+	Folder    int64  `json:"folder"`
+	ID        int64  `json:"id"`
+	Modified  int64  `json:"modified"`
+	Priority  int    `json:"priority"`
+	Star      int    `json:"star"`
+	Title     string `json:"title"`
+}
