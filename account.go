f8926a6250115072a2313dd4dfce25f59ef589ab
aditya <dev@chimeracoder.net>
2014-09-27 14:29:21 -0400

    Add Account struct definition

---
+++ account.go
@@ -0,0 +1,26 @@
+package toodledo
+
+type Account struct {
+	Alias            string  `json:"alias"`
+	DateFormat       int `json:"dateformat"`
+	Email            string  `json:"email"`
+	HideMonths       int `json:"hidemonths"`
+	HotListduedate   int `json:"hotlistduedate"`
+	HotListpriority  int `json:"hotlistpriority"`
+	HotListstar      int `json:"hotliststar"`
+	HotListstatus    int `json:"hotliststatus"`
+	LastDeleteNote   int `json:"lastdelete_note"`
+	LastDeleteTask   int `json:"lastdelete_task"`
+	LastEditContext  int `json:"lastedit_context"`
+	LastEditFolder   int `json:"lastedit_folder"`
+	LastEditGoal     int `json:"lastedit_goal"`
+	LastEditList     int `json:"lastedit_list"`
+	LastEditLocation int `json:"lastedit_location"`
+	LastEditNote     int `json:"lastedit_note"`
+	LastEditOutline  int `json:"lastedit_outline"`
+	LastEditTask     int `json:"lastedit_task"`
+	Pro              int `json:"pro"`
+	ShowTabNums      int `json:"showtabnums"`
+	Timezone         int `json:"timezone"`
+	Userid           string  `json:"userid"`
+}
